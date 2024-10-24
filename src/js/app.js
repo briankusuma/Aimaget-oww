3f3955e12a973bce56cfd32f72533e622a147dc1
briankusuma <kusuma.brian@gmail.com>
2024-10-24 16:10:52 +0700

    build slider galery

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
 console.log("Its live!!");
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 5,
+    autoplay: true,
+    loop: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: -100,
+        depth: 200,
+        modifier: 0.4,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: '.swiper-wrapper-button-next',
+        prevEl: '.swiper-wrapper-button-prev',
+    },
+});
